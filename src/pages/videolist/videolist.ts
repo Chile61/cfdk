4a320bb8729ef4966a965a3c298bda5795387a50
Devon <k849996781@vip.qq.com>
2016-11-04 20:30:47 +0800

    2016-11-04

---
+++ src/pages/videolist/videolist.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-videolist',
+  templateUrl: 'videolist.html'
+})
+export class videolistPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
